b8e8221d65949a76a02bf37e2cea71c1232f5896
elisabet <edacalm990@g.educaand.es>
2022-11-07 14:18:52 +0100

    morra

---
+++ src/main/java/ejB/Password.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package ejB;
+import org.apache.commons.lang3.RandomStringUtils;
+/**
+ *
+ * @author elisabet
+ */
+public class Password {
+    public static void main(String[] args) {
+        
+    }
+}
